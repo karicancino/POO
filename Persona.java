5a8022514b860935aea676f66ebf0e53a21f8ee7
KARINA CANCINO <92641519+karicancino@users.noreply.github.com>
2022-03-24 11:48:32 -0600

    Create Persona.java

---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona{
+    public String curp;
+    public String nombre;
+    public String direccion;
+    public String genero;
+    
+    public Persona()
+    {
+        curp=null;
+        nombre=null;
+        direccion=null;
+        genero=null;
+    }
+    
+}
